9aeaaf26da74f0a1481a43a71c2dc5468af6a965
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2021-08-30 13:14:24 +0200

    converting to TS

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,11 +1,18 @@
 const API_URL = 'https://jsonplaceholder.typicode.com/';
 
-export async function getPostsData() {
+export interface Post {
+    id: number,
+    title: string,
+    body: string
+}
+
+export async function getPostsData(): Promise<Post[]> {
     // Instead of the file system,
     // fetch post data from an external API endpoint
     const res = await fetch(API_URL + 'posts?_limit=10');
     return res.json()
 }
+
 export async function getAllPostIds() {
     const posts = await getPostsData()
 
@@ -16,7 +23,7 @@ export async function getAllPostIds() {
     }))
 }
 
-export async function getPostData(id) {
+export async function getPostData(id: string): Promise<Post> {
     const res = await fetch(API_URL + `posts/${id}`)
     return res.json()
 }
